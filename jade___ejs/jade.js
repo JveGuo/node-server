e42cf0536a486b78b008c0552cd61a969ece9b3e
jveguo <jveguo16@gmail.com>
2017-09-28 22:07:03 +0800

    jade

--- jade___ejs/jade.js
+++ jade___ejs/jade.js
@@ -1,3 +1,9 @@
 const jade = require('jade');
+const fs = require('fs');
 
 var str = jade.renderFile('./views/1.jade',{pretty: true});
+
+fs.writeFile('./',str,function(err){
+  if(err)
+    console.log('写入失败');
+})
